dbbcc42d3e568fde188b0255d7fa4351bc88c3ba
Gammenon <159122531+Gammen0n@users.noreply.github.com>
2025-01-09 11:25:28 +0100

    ajout db1

--- src/capteurtempjava/Consumer.java
+++ src/capteurtempjava/Consumer.java
@@ -1,13 +1,16 @@
+import java.sql.DriverManager;
+
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
 public class Consumer {
-
 	private static final String EXCHANGE_NAME = "logs";
-
 	private static final String BROKER_HOST = System.getenv("broker_host");
+
+	private static final String driver = "org.gjt.mm.mysql.Driver";
+    private static Connection singleton = null;
 
 	public static void main(String[] argv) throws Exception {
 		ConnectionFactory factory = new ConnectionFactory();
@@ -29,5 +32,26 @@ public class Consumer {
 		channel.basicConsume(queueName, true, deliverCallback, consumerTag -> {
 		});
 	}
+    
+    public static Connection getConnexion() throws Exception { 
+        if (singleton == null){ 
+            Class.forName(driver); 
+            singleton =DriverManager.getConnection("jdbc:mysql:./DBcapteurs","root","root");
+        }
+        return (singleton); 
+    }
+
+	public void insérerTemp() throws Exception{
+        String sql;
+        Statement objRequete;
+        objRequete = GestionConnexion.getConnexion().createStatement();
+        sql = "INSERT INTO `Temperature`(temperature, date) VALUES ("+this.temperature+",'"+this.date+"')";
+        objRequete.executeUpdate(sql);
+    }
+    
+    public static void liberer() throws Exception { 
+        singleton.close();
+        singleton = null;
+    }
 
 }
